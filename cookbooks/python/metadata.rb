d5fcff4c6c6f17509cccbec937673db2fabc0052
Corey Donohoe <atmos@atmos.org>
2010-05-25 15:32:26 -0700

    add support for python from homebrew as well

---
+++ cookbooks/python/metadata.rb
@@ -0,0 +1,6 @@
+maintainer       "atmos"
+maintainer_email "atmos@atmos.org"
+license          "MIT"
+description      "Installs/Configures python"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
+version          "0.1"
---
+++ cookbooks/python/recipes/default.rb
@@ -0,0 +1,11 @@
+#
+# Cookbook Name:: python
+# Recipe:: default
+#
+
+root = File.expand_path(File.join(File.dirname(__FILE__), "..", "..", "homebrew"))
+
+require root + "/resources/homebrew"
+require root + "/providers/homebrew"
+
+homebrew "python"
