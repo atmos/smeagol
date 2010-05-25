12f3d41759df6d9205778bd056d12d133259b518
Corey Donohoe <atmos@atmos.org>
2010-05-25 11:16:12 -0700

    include npm in the default builds as well

--- cookbooks/node/recipes/default.rb
+++ cookbooks/node/recipes/default.rb
@@ -13,6 +13,26 @@ require root + '/providers/homebrew'
   homebrew pkg
 end
 
-#execute "fetch the latest npm" do
-  #command "http://github.com/isaacs/npm/tarball/master"
-#end
+### Installing npm is kinda painful
+npm_executable = "#{ENV['HOME']}/Developer/bin/npm"
+file npm_executable do
+  owner ENV['USER']
+  group "staff"
+  mode "0755"
+  action :create
+end
+
+template npm_executable do
+  source "npm.erb"
+end
+
+directory "#{ENV['HOME']}/Developer/Cellar/npm" do
+  owner ENV['USER']
+  group "staff"
+  mode "0755"
+end
+
+execute "fetching latest stable npm release" do
+  command "curl -L http://github.com/isaacs/npm/tarball/master | tar xz --strip 1; make"
+  cwd "#{ENV['HOME']}/Developer/Cellar/npm"
+end
