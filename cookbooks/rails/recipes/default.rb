f79abc0ff20760051ba2b3ab238822fae2966d0c
Corey Donohoe <atmos@atmos.org>
2010-05-30 13:10:49 -0700

    add the ey and heroku gems for easier deployment

--- cookbooks/rails/recipes/default.rb
+++ cookbooks/rails/recipes/default.rb
@@ -2,8 +2,10 @@
 # Cookbook Name:: rails
 # Recipe:: default
 #
-RAILS_2_VERSION = "2.3.8"
-RAILS_3_VERSION = "3.0.0.beta3"
+EY_GEM_VERSION      = "0.3.2"
+RAILS_2_VERSION     = "2.3.8"
+RAILS_3_VERSION     = "3.0.0.beta3"
+HEROKU_GEM_VERSION  = "1.9.9"
 
 script "updating to the latest rails version #{RAILS_2_VERSION}" do
   interpreter "bash"
@@ -23,6 +25,28 @@ script "installing the latest rails3 beta #{RAILS_3_VERSION}" do
     `gem list rails | grep -q '#{RAILS_3_VERSION}'`
     if [[ $? -ne 0 ]]; then
       gem install rails --version=#{RAILS_3_VERSION} --pre --no-rdoc --no-ri
+    fi
+  EOS
+end
+
+script "installing the latest engineyard gem" do
+  interpreter "bash"
+  code <<-EOS
+    source ~/.cider.profile
+    `gem list engineyard | grep -q '#{EY_GEM_VERSION}'`
+    if [[ $? -ne 0 ]]; then
+      gem install engineyard --version=#{EY_GEM_VERSION} --no-rdoc --no-ri
+    fi
+  EOS
+end
+
+script "installing the latest heroku gem" do
+  interpreter "bash"
+  code <<-EOS
+    source ~/.cider.profile
+    `gem list heroku | grep -q '#{HEROKU_GEM_VERSION}'`
+    if [[ $? -ne 0 ]]; then
+      gem install heroku --version=#{HEROKU_GEM_VERSION} --no-rdoc --no-ri
     fi
   EOS
 end
--- cookbooks/rvm/recipes/default.rb
+++ cookbooks/rvm/recipes/default.rb
@@ -61,3 +61,23 @@ end
     EOS
   end
 end
+
+{ 'wirble' => '0.3.1', 'awesome_print' => '0.2.0', 'hirb' => '0.3.2',
+  'what_methods' => '1.0.1', 'looksee' => '0.2.1', 'sketches' => '0.1.1',
+  'net-http-spy' => '0.2.1', 'map_by_method' => '0.8.3'
+  }.each do |name, version|
+  script "installed #{name}(#{version}) to liven up your ~/.irbrc a little" do
+    interpreter "bash"
+    code <<-EOS
+      source ~/.cider.profile
+      `gem list #{name} | grep -q '#{version}'`
+      if [[ $? -ne 0 ]]; then
+        gem install #{name} --no-rdoc --no-ri
+      fi
+    EOS
+  end
+end
+
+template "#{ENV['HOME']}/.irbrc" do
+  source "dot.irbrc.erb"
+end
