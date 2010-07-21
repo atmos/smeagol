1ed536fd352701475e7622edaa0e4b7eff543a21
Corey Donohoe <atmos@atmos.org>
2010-07-21 12:37:43 -0700

    include imagemagick and rmagick by default
    confirmed working on 1.8 and 1.9

--- cookbooks/homebrew/recipes/misc.rb
+++ cookbooks/homebrew/recipes/misc.rb
@@ -8,6 +8,6 @@ root = File.expand_path(File.join(File.dirname(__FILE__), ".."))
 require root + '/resources/homebrew'
 require root + '/providers/homebrew'
 
-%w(tig ack sqlite wget fortune proctools markdown ctags-exuberant).each do |pkg|
+%w(tig ack imagemagick sqlite wget fortune proctools markdown ctags-exuberant).each do |pkg|
   homebrew pkg
 end
