f2a7070d47903c42c614abd07091041d73a0d468
Corey Donohoe <atmos@atmos.org>
2011-03-13 23:16:00 -0700

    move to nvm for node/npm management

--- cookbooks/node/recipes/default.rb
+++ cookbooks/node/recipes/default.rb
@@ -10,11 +10,6 @@ require root + '/providers/homebrew'
 require 'etc'
 
 
-### install a bunch of utils
-%w(rlwrap node npm rlwrap).each do |pkg|
-  homebrew pkg
-end
-
 script "configuring ndistro" do
   interpreter "bash"
   code <<-EOS
@@ -23,5 +18,20 @@ script "configuring ndistro" do
 
     curl -# -L http://github.com/visionmedia/ndistro/raw/master/bin/ndistro > ndistro 2> ~/.cinderella.log
     chmod 0755 ndistro
+  EOS
+end
+
+script "configuring nvm" do
+  interpreter "bash"
+  code <<-EOS
+    source ~/.cinderella.profile
+    cd #{ENV['HOME']}/Developer
+    if [ ! -d ./.nvm ]; then
+      git clone git://github.com/creationix/nvm.git .nvm >> ~/.cinderella/bootstrap.log
+      source #{ENV['HOME']}/Developer/.nvm/nvm.sh        >> ~/.cinderella/bootstrap.log
+      nvm install v0.4.1                                 >> ~/.cinderella/bootstrap.log
+      nvm use v0.4.1                                     >> ~/.cinderella/bootstrap.log
+      curl http://npmjs.org/install.sh | sh              >> ~/.cinderella/bootstrap.log
+    fi
   EOS
 end
