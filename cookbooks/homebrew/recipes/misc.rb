235d06c281bfb50509358e01e17612e705acdd57
Corey Donohoe <atmos@atmos.org>
2011-04-05 09:40:09 -0700

    include the brew-services executable

--- cookbooks/homebrew/recipes/misc.rb
+++ cookbooks/homebrew/recipes/misc.rb
@@ -11,3 +11,10 @@ require root + '/providers/homebrew'
 %w(tig ack coreutils imagemagick sqlite gist wget hub fortune proctools markdown ctags-exuberant).each do |pkg|
   homebrew pkg
 end
+
+
+file "#{ENV['HOME']}/Developer/bin/brew-services" do
+  mode   "0755"
+  action :create
+  content open("https://gist.github.com/raw/766293/75a7907004bbff0eb3b072d1d951be2cfe7e5020/brew-services.rb").read
+end
