2b8371a6709d3fa8adfb0d0ee3bfa4a16d929714
Corey Donohoe <atmos@atmos.org>
2011-08-15 16:32:13 -0700

    update nvm to behave well too

--- cookbooks/node/recipes/default.rb
+++ cookbooks/node/recipes/default.rb
@@ -9,17 +9,25 @@ require root + '/resources/homebrew'
 require root + '/providers/homebrew'
 require 'etc'
 
+template "#{ENV['HOME']}/.npmrc" do
+  mode   0700
+  owner  ENV['USER']
+  group  Etc.getgrgid(Process.gid).name
+  source "dot.npmrc.erb"
+  variables({ :home => ENV['HOME'] })
+end
+
 script "configuring nvm" do
   interpreter "bash"
   code <<-EOS
     source ~/.cinderella.profile
     cd #{ENV['HOME']}/Developer
     if [ ! -d ./.nvm ]; then
-      git clone git://github.com/creationix/nvm.git .nvm >> ~/.cinderella/bootstrap.log
-      source #{ENV['HOME']}/Developer/.nvm/nvm.sh        >> ~/.cinderella/bootstrap.log
-      nvm install v0.4.1                                 >> ~/.cinderella/bootstrap.log
-      nvm use v0.4.1                                     >> ~/.cinderella/bootstrap.log
-      curl http://npmjs.org/install.sh | sh              >> ~/.cinderella/bootstrap.log
+      git clone git://github.com/creationix/nvm.git .nvm >> ~/.cinderella/node.log
+      source #{ENV['HOME']}/Developer/.nvm/nvm.sh        >> ~/.cinderella/node.log
+      nvm install v0.4.10                                >> ~/.cinderella/node.log
+      nvm alias default v0.4.10                          >> ~/.cinderella/node.log
+      curl http://npmjs.org/install.sh | clean=yes sh    >> ~/.cinderella/node.log
     fi
   EOS
 end
