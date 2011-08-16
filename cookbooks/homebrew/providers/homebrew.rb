86c47128efef3b891ffb3f8631440211cb0dfc27
Corey Donohoe <atmos@atmos.org>
2011-08-15 20:12:28 -0700

    cleanup busted mysql_install_db

--- cookbooks/homebrew/providers/homebrew.rb
+++ cookbooks/homebrew/providers/homebrew.rb
@@ -66,7 +66,7 @@ class Chef
               system("#{PREFIX}/bin/initdb #{PREFIX}/var/postgres > /dev/null 2>&1")
             end
           when "mysql"
-            unless ::File.directory?("#{PREFIX}/var/mysql")
+            unless ::File.directory?("#{PREFIX}/var/mysql/mysql")
               prefix = `brew --prefix mysql`.chomp
               system("unset TMPDIR; #{PREFIX}/bin/mysql_install_db --user=#{ENV['USER']} --basedir=#{prefix} --datadir=#{PREFIX}/var/mysql --tmpdir=/tmp > /dev/null")
             end
