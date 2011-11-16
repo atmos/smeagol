cab692693b736fc10f347c3a477a371bc404a2be
Corey Donohoe <atmos@atmos.org>
2011-11-16 14:47:59 -0800

    workaround bad md5 for distribute

--- cookbooks/python/recipes/default.rb
+++ cookbooks/python/recipes/default.rb
@@ -8,6 +8,12 @@ root = File.expand_path(File.join(File.dirname(__FILE__), "..", "..", "homebrew"
 require root + "/resources/homebrew"
 require root + "/providers/homebrew"
 
+execute "downloading proper distribute package" do
+  command "wget http://cinderella.s3.amazonaws.com/distribute-0.6.21.tar.gz"
+  cwd "#{ENV['HOME']}/Library/Caches/Homebrew"
+  not_if "test -e #{ENV['HOME']}/Library/Caches/Homebrew/distribute-0.6.21.tar.gz"
+end
+
 homebrew "python"
 
 # execute "install pip" do
