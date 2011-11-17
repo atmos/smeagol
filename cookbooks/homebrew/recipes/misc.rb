ca231de069ae5770772de0f6d19e2c7859bd0e91
Corey Donohoe <atmos@atmos.org>
2011-11-16 17:38:19 -0800

    wget shit

--- cookbooks/homebrew/recipes/misc.rb
+++ cookbooks/homebrew/recipes/misc.rb
@@ -8,7 +8,7 @@ root = File.expand_path(File.join(File.dirname(__FILE__), ".."))
 require root + '/resources/homebrew'
 require root + '/providers/homebrew'
 
-%w(tig ack coreutils imagemagick sqlite wget fortune proctools markdown ctags-exuberant gpgme icu4c).each do |pkg|
+%w(tig ack coreutils imagemagick sqlite wget fortune proctools markdown ctags-exuberant gpgme icu4c wget).each do |pkg|
   homebrew pkg
 end
 
