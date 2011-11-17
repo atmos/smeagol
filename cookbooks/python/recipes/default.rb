c421c583ce1ef24fb100c219d7d07a86577fe2c3
Corey Donohoe <atmos@atmos.org>
2011-11-16 18:14:42 -0800

    make sure you can access wget

--- cookbooks/python/recipes/default.rb
+++ cookbooks/python/recipes/default.rb
@@ -8,10 +8,13 @@ root = File.expand_path(File.join(File.dirname(__FILE__), "..", "..", "homebrew"
 require root + "/resources/homebrew"
 require root + "/providers/homebrew"
 
-execute "downloading proper distribute package" do
-  command "wget http://cinderella.s3.amazonaws.com/distribute-0.6.21.tar.gz"
-  cwd "#{ENV['HOME']}/Library/Caches/Homebrew"
-  not_if "test -e #{ENV['HOME']}/Library/Caches/Homebrew/distribute-0.6.21.tar.gz"
+script "downloading proper distribute package" do
+  interpreter "bash"
+  code <<-EOS
+    source ~/.cinderella.profile
+    cd #{ENV['HOME']}/Library/Caches/Homebrew
+    wget http://cinderella.s3.amazonaws.com/distribute-0.6.21.tar.gz
+  EOS
 end
 
 homebrew "python"
